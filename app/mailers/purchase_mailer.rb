48eeaf2112cfda297b2328a40c42d2a783f46216
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-01-30 13:15:16 -0800

    receipt updates

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,7 +1,7 @@
 class PurchaseMailer < ActionMailer::Base
 layout 'purchase_mailer'
 
- default from: "Burrito Genie (satisfy@burritogenie.com)"
+ default from: "Burrito Genie Order (yourorder@burritogenie.com)"
 
  def purchase_receipt purchase
  @purchase = purchase
