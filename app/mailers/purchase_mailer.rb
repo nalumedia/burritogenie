0c8c80d0889ebd0f7a2341ce21f01a78ae0a9de8
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-01-30 12:41:47 -0800

    getting real on email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -5,7 +5,8 @@ layout 'purchase_mailer'
 
  def purchase_receipt purchase
  @purchase = purchase
- mail to: purchase.email, subject: "Your Burrito Is On The Way!"
+ mail to: purchase.email, subject: "Your Burrito Is On The Way!", bcc: "burritogenie@nalumedia.com"
+
 
   end
 
