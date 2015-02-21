d3041ced1260950988045ad1b61128d787696d3c
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-02-21 07:49:17 -0800

    updated contacts

--- app/controllers/delivertos_controller.rb
+++ app/controllers/delivertos_controller.rb
@@ -1,5 +1,15 @@
-class DelivertosController < InheritedResources::Base
+class DelivertosController < ApplicationController
 				before_action :authenticate_user!, except: [:show]
+				before_filter :correct_user, :only => [:destroy]
+
+	def index 
+			@delivertos = Deliverto.all	
+	end 
+
+	def show 
+			@delivertos = Deliverto.all	
+	end 
+
 	def new 
 			@deliverto = Deliverto.new
 			@delivertostatuses = Delivertostatus.all
@@ -10,10 +20,37 @@ class DelivertosController < InheritedResources::Base
 			@delivertostatuses = Delivertostatus.all
 	end 
 
+	def update
+    respond_to do |format|
+      if @deliverto.update(deliverto_params)
+        format.html { redirect_to @deliverto, notice: 'Deliverto was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @deliverto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    def destroy
+			    @deliverto = Deliverto.find(params[:id])
+			    if @deliverto.present?
+			    @deilverto.destroy
+			    end
+			    redirect_to delivertos_path
+			    end
+ 
+
 
 
 
   private
+  	def set_deliverto
+      @deliverto = Deliverto.find(params[:id])
+    end
+
+    
+
 
     def deliverto_params
       params.require(:deliverto).permit(:deliverto_street, :deliverto_zip, :deliverto_phone, :deliverto_instructions, :deliverto, :delivertostatus_id)
