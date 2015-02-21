8f9032b24540db3e8501ceba18c1b15dad8c5c35
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-02-21 09:19:40 -0800

    removed image

--- app/models/burritolocation.rb
+++ app/models/burritolocation.rb
@@ -1,7 +1,4 @@
 class Burritolocation < ActiveRecord::Base
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-        validates :image, presence: true
-        validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 
 end
